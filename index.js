16216cb0772e773f785efc59f6d5cddad3796590
Ohi97 <ashrafohi20@gmail.com>
2022-07-06 15:13:12 +0600

    Toastify-Added

--- index.js
+++ index.js
@@ -16,12 +16,24 @@ async function run(){
     try{
         await client.connect();
         const servicesCollection = client.db('camera-store').collection('services');
+        const bookingsCollection = client.db('booking').collection('services');
 
         app.get('/service', async(req, res) => {
             const query = {};
             const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        app.post('/booking', async(req, res) => {
+          const booking = req.body;
+          const query = {appointments: booking.appointments, date: booking.date, user: booking.user}
+          const exists = await bookingsCollection.findOne(query);
+          if(exists){
+            return res.send({success: false, booking: exists})
+          }
+          const result = await bookingsCollection.insertOne(booking);
+          return res.send({success: true, result});
         })
     }
     finally{
